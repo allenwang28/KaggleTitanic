58f9d0aef096b00cff1514165e9b82d3ba57d477
Allen Wang <allenwang@utexas.edu>
2015-06-20 20:07:20 -0500

    Initial commit. No code written yet

---
+++ ref/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import pylab as P
+import decimal
+import csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.cross_validation import train_test_split
+from sklearn import cross_validation
+
